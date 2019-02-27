740adca75207ef4c80edb55b53fb79d94772836e
DeveloperKHJ <dbtmd1007gh1@gmail.com>
2019-02-27 15:57:09 +0900

    fix

--- clock.js
+++ clock.js
@@ -3,10 +3,9 @@ const clockTitle = clockContainer.querySelector("h1");
 
 function getTime(){
   const date = new Date();
-  
   const minutes = date.getMinutes();
   const hours = date.getHours();
-  const second = date.getSecond();
+  const second = date.getSeconds();
   clockTitle.innerText = `${hours}:${minutes}`;
 }
 
